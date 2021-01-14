f05659ce1eefcd15a227a9718328a15b377ef258
Olga Jaeger <olgar.mamaeva@rambler.ru>
2021-01-14 17:37:13 +0100

    http - create database and add POST request

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,45 @@
 <template>
-  <div>
+  <div class="container">
+    <form class="card" @submit.prevent="createPerson">
+      <h2>Работа с базой данных</h2>
 
+      <div class="form-control" >
+        <label for="name">Add name</label>
+        <input type="text" id="name" v-model.trim="name">
+      </div>
+
+      <button class="btn primary" :disabled="name.length === 0" >Create person</button>
+
+    </form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
-  components: {
+  data () {
+    return {
+      name: ''
+    }
+  },
+  methods: {
+    async createPerson () {
+      // https://vue-http-44ded-default-rtdb.europe-west1.firebasedatabase.app/people.json
+      // this.name
+
+      const response = await fetch('https://vue-http-44ded-default-rtdb.europe-west1.firebasedatabase.app/people.json', {
+        method: 'POST',
+        headers: {
+          'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+          firstName: this.name
+        })
+      })
+
+      const firebaseData = await response.json()
+      console.log(firebaseData)
+      this.name = ''
+    }
   }
 }
 </script>
