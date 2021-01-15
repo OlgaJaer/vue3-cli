825694780e07361c3adaa36465e31a7e3d4c171c
Olga Jaeger <olgar.mamaeva@rambler.ru>
2021-01-15 14:21:46 +0100

    http_1 - create component for list of people, add GET request

--- src/App.vue
+++ src/App.vue
@@ -11,21 +11,28 @@
       <button class="btn primary" :disabled="name.length === 0" >Create person</button>
 
     </form>
+    <app-people-list
+    :people="people"
+    @load="loadPeople"
+    ></app-people-list>
   </div>
 </template>
 
 <script>
+import AppPeopleList from './AppPeopleList'
+// eslint-disable-next-line no-unused-vars
+import axios from 'axios'
+
 export default {
   data () {
     return {
-      name: ''
+      name: '',
+      people: []
     }
   },
+  components: { AppPeopleList },
   methods: {
     async createPerson () {
-      // https://vue-http-44ded-default-rtdb.europe-west1.firebasedatabase.app/people.json
-      // this.name
-
       const response = await fetch('https://vue-http-44ded-default-rtdb.europe-west1.firebasedatabase.app/people.json', {
         method: 'POST',
         headers: {
@@ -39,8 +46,22 @@ export default {
       const firebaseData = await response.json()
       console.log(firebaseData)
       this.name = ''
+    },
+    async loadPeople () {
+      const { data } = await axios.get('https://vue-http-44ded-default-rtdb.europe-west1.firebasedatabase.app/people.json')
+      // console.log(data)
+      const result = Object.keys(data).map(key => {
+        return {
+          id: key,
+          // firstName: data[key].firstName
+          ...data[key]
+        }
+      })
+      // console.log(result)
+      this.people = result
     }
   }
+
 }
 </script>
 
