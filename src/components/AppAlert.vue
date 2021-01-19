8bb14be0fe1f5332213432a0a8a608fafd172272
Olga Jaeger <olgar.mamaeva@rambler.ru>
2021-01-19 17:30:27 +0100

    create component Alert

---
+++ src/components/AppAlert.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="alert" :class="type">
+    <h2 class="alert-title">{{title}}</h2>
+    <p>{{text}}</p>
+    <button class="btn" :class='type' v-if='closeble' @click="$emit('close')">{{close_text}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  emits: ['close'],
+  props: {
+    title: String,
+    text: String,
+    closeble: {
+      type: Boolean,
+      required: false,
+      default: false
+    },
+    close_text: String,
+    type: {
+      type: String,
+      default: 'primary',
+      required: false,
+      validator (value) {
+        return ['warning', 'primary', 'danger'].includes(value)
+      }
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/AppBlock.vue
+++ src/components/AppBlock.vue
@@ -4,22 +4,17 @@
       v-if="alert"
       title='Atention!'
       text='This is a very important message'
-      type='warning'
       close_text='Close'
-      :closeble='true'
       @close="alert = false"
     ></app-alert>
     <div class="card">
       <button class="btn primary" @click="toggleAlert">{{alert ? "hide" : 'Show'}} message</button>
     </div>
-
-  <app-block></app-block>
   </div>
 </template>
 
 <script>
-import AppAlert from './components/AppAlert'
-import AppBlock from './components/AppBlock'
+import AppAlert from './AppAlert'
 export default {
   data () {
     return {
@@ -32,6 +27,6 @@ export default {
     }
 
   },
-  components: { AppAlert, AppBlock }
+  components: { AppAlert }
 }
 </script>
