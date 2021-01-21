aedd46d91ecd829d99e44f70e0ace8323d76112c
Olga Jaeger <olgar.mamaeva@rambler.ru>
2021-01-21 18:26:40 +0100

    Modal component with teleport

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,30 @@
   <div class="card">
     <h2>{{ $i18n('app.title') }}</h2>
 
-  <nutton class="btn" @click="changeLang">{{$i18n('app.changeBtn')}}</nutton>
+  <button class="btn" @click="changeLang">{{$i18n('app.changeBtn')}}</button>
+  <button class="btn primary" @click="modal = true">Open Modal</button>
+  <!-- <keep-alive></keep-alive> -->
+  <teleport to="body">
+    <app-modal v-if="modal" @close="modal=false"></app-modal>
+  </teleport>
   </div>
 </template>
 
 <script>
-
+import AppModal from './components/AppModal'
 export default {
+  data () {
+    return {
+      modal: false
+    }
+  },
   inject: ['changeI18N'],
   methods: {
     changeLang () {
       this.changeI18N('en')
       this.$forceUpdate()
     }
-
-  }
+  },
+  components: { AppModal }
 }
 </script>
---
+++ src/components/AppModal.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="modal-backdrop" @click="$emit('close')">
+  <div class="modal">
+    <h3>Title Modal</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Similique neque quaerat exercitationem magni ratione molestias, ea vel voluptates eos quidem quod magnam iure, accusamus enim inventore quos animi quasi provident!</p>
+  </div>
+
+</div>
+
+</template>
+
+<script>
+export default {
+  emits: ['close']
+
+}
+</script>
+
+<style>
+
+</style>
