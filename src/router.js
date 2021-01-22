decf1b4fba9bc098e3e10dfc20d92bd0d50a126e
Olga Jaeger <olgar.mamaeva@rambler.ru>
2021-01-22 20:59:18 +0100

    Navigation Guards

--- src/router.js
+++ src/router.js
@@ -6,14 +6,21 @@ import Mail from './views/Mail.vue'
 import AppEmailBody from './components/AppEmailBody.vue'
 import NotFound from './views/NotFound.vue'
 
-export default createRouter({
+const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/login', component: Login, alias: '/' },
-    { path: '/forget', component: Forget },
-    { path: '/dashboard', component: Dashboard },
+    { path: '/forget', component: Forget, meta: { cantEnter: true } },
     {
-      path: '/mail/',
+      path: '/dashboard',
+      component: Dashboard,
+      name: 'home',
+      beforeEnter () {
+        console.log('beforeEnter')
+      }
+    },
+    {
+      path: '/mail',
       component: Mail,
       children: [
         { path: ':mailId?', component: AppEmailBody, props: true }
@@ -24,3 +31,23 @@ export default createRouter({
   linkActiveClass: 'active',
   linkExactActiveClass: 'active'
 })
+
+router.beforeEach((to, from, next) => {
+  // console.log('to', to)
+  // console.log('from', from)
+  console.log('beforeEach')
+  if (to.meta.cantEnter) {
+    // next('/mail')
+    next({ name: 'home' })
+  } else {
+    next()
+  }
+
+  next()
+})
+
+router.afterEach((to, from) => {
+
+})
+
+export default router
