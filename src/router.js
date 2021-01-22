a1f4f35ac87c753b16542f7d32c37c22db9110b3
Olga Jaeger <olgar.mamaeva@rambler.ru>
2021-01-22 18:17:58 +0100

    Nested Routes

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Login from './views/Login.vue'
 import Forget from './views/Forget.vue'
 import Dashboard from './views/Dashboard.vue'
 import Mail from './views/Mail.vue'
+import AppEmailBody from './components/AppEmailBody.vue'
 
 export default createRouter({
   history: createWebHistory(),
@@ -10,7 +11,13 @@ export default createRouter({
     { path: '/login', component: Login, alias: '/' },
     { path: '/forget', component: Forget },
     { path: '/dashboard', component: Dashboard },
-    { path: '/mail/:mailId?', component: Mail } // ? необязательный парам
+    {
+      path: '/mail/',
+      component: Mail,
+      children: [
+        { path: ':mailId?', component: AppEmailBody, props: true }
+      ]
+    }
   ],
   linkActiveClass: 'active',
   linkExactActiveClass: 'active'
