a92b7c7869ef8f6b57627a93684e8f87750ac91d
Olga Jaeger <olgar.mamaeva@rambler.ru>
2021-01-22 18:57:07 +0100

    page 404

--- src/router.js
+++ src/router.js
@@ -4,6 +4,7 @@ import Forget from './views/Forget.vue'
 import Dashboard from './views/Dashboard.vue'
 import Mail from './views/Mail.vue'
 import AppEmailBody from './components/AppEmailBody.vue'
+import NotFound from './views/NotFound.vue'
 
 export default createRouter({
   history: createWebHistory(),
@@ -17,7 +18,8 @@ export default createRouter({
       children: [
         { path: ':mailId?', component: AppEmailBody, props: true }
       ]
-    }
+    },
+    { path: '/:notFound(.*)', component: NotFound }
   ],
   linkActiveClass: 'active',
   linkExactActiveClass: 'active'
---
+++ src/views/NotFound.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="card">
+    <h1>Page is not found</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. </p>
+    <router-link to="/login">To Main Page</router-link>
+  </div>
+</template>
