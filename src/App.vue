c384c809f0c2f9265eb3b5d75b327c5255452ec9
Olga Jaeger <olgar.mamaeva@rambler.ru>
2021-01-22 22:23:08 +0100

    create store

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,28 @@
 <template>
-  <div>
-
+  <the-navbar></the-navbar>
+  <div class="container with-nav">
+    <div class="card">
+      <h1>Про Vuex</h1>
+      <h2>Счетчик {{$store.state.counter}}</h2>
+      <button class="btn" @click="inc">Add</button>
+    </div>
   </div>
 </template>
 
 <script>
+import TheNavbar from './TheNavbar.vue'
+
 export default {
-  name: 'App',
-  components: {
+  components: { TheNavbar },
+  // data () {
+  //   return {
+  //     counter: 0
+  //   }
+  // },
+  methods: {
+    inc () {
+      this.$store.state.counter++
+    }
   }
 }
 </script>
-
-<style>
-
-</style>
---
+++ src/TheNavbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <header class="navbar">
+    Vuex
+  </header>
+</template>
+
+<script>
+export default {
+}
+</script>
